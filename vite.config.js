c6b8dffbe488025a727999121b2bb7c650f4cf67
Anas Hersi <101137632+kugicode@users.noreply.github.com>
2025-10-17 16:21:48 +0100

    "Added base for github pages to work."

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
+// vite.config.js
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // **<-- Add this line to fix the GitHub Pages path!**
+  base: '/frontend-coursework1/',
   plugins: [
     vue(),
     vueDevTools(),
